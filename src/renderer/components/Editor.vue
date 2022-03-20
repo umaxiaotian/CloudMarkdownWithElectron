1a8f7ef412c0550041d26565203bea3aa9e3a4d4
小畑佑馬 <yuumaobata1008@outlook.com>
2022-03-20 12:19:34 +0900

    no message

--- src/renderer/components/Editor.vue
+++ src/renderer/components/Editor.vue
@@ -17,8 +17,8 @@
         </div>
       </pane>
       <pane :size="100 - paneSize">
-        <div id="rightView">
-          <Viewarea :markdown="markdown" />
+        <div>
+          <Viewarea id="rightView" :markdown="markdown" />
         </div>
       </pane>
     </splitpanes>
@@ -26,15 +26,15 @@
 </template>
 
 <script>
-import { Splitpanes, Pane } from 'splitpanes'
-import 'splitpanes/dist/splitpanes.css'
-import Textarea from '@/components/Textarea'
-import Viewarea from '@/components/Viewarea'
-const { ipcRenderer } = require('electron')
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
+import Textarea from "@/components/Textarea";
+import Viewarea from "@/components/Viewarea";
+const { ipcRenderer } = require("electron");
 export default {
-  name: 'Editor',
+  name: "Editor",
   components: { Splitpanes, Pane, Textarea, Viewarea },
-  data () {
+  data() {
     return {
       paneSize: 50,
       editorScrollTop: 0,
@@ -62,42 +62,44 @@ export default {
 ###### h6 Heading
 
 
-      `
-    }
+      `,
+    };
   },
-  created () {
+  created() {
     // IPCでメッセージを受信してファイルの制御を行う
-    ipcRenderer.on('main_file_message', (event, arg) => {
-      console.log(arg)
+    ipcRenderer.on("main_file_message", (event, arg) => {
+      console.log(arg);
       switch (arg) {
-      case 'open':
-        // ファイルを開く
-        // loadFile();
-        break
-      case 'save':
-        // ファイルを保存
-        // saveFile();
-        break
-      case 'saveas':
-        // 名前を付けてファイルを保存
-        // saveNewFile();
-        break
+        case "open":
+          // ファイルを開く
+          // loadFile();
+          break;
+        case "save":
+          // ファイルを保存
+          // saveFile();
+          break;
+        case "saveas":
+          // 名前を付けてファイルを保存
+          // saveNewFile();
+          break;
       }
-    })
+    });
   },
   methods: {
-    editorScroll (val) {
-      const rView = document.getElementById('rightView')
+    editorScroll(val) {
+      const rView = document.getElementById("rightView");
+
       // スクロール比率を計算　（100%が1）
-      const scrollRatio = val.scrollTop / (val.scrollHeight - val.clientHeight)
+      const scrollRatio = val.scrollTop / (val.scrollHeight - val.clientHeight);
+
       // 比率をもとに書き込み
-      rView.scrollTop = (rView.scrollHeight - rView.clientHeight) * scrollRatio
+      rView.scrollTop = (rView.scrollHeight - rView.clientHeight) * scrollRatio;
     },
     // 文字列を更新
-    inputEditor (contents) {
-      console.log(contents)
-      this.markdown = contents
-    }
-  }
-}
+    inputEditor(contents) {
+      console.log(contents);
+      this.markdown = contents;
+    },
+  },
+};
 </script>
